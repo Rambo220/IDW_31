a15c6af1949d80073d62b769b560f436f6cc8561
Martin Mansilla <106232183+Rambo220@users.noreply.github.com>
2025-10-22 20:45:02 -0300

    Revert "Actualizacion 3er entrega"
    This reverts commit 63e0e2dcacf6f9adbe7c96003bfbafaaa00afdc6.

--- common/js/app/main.js
+++
@@ -1,5 +0,0 @@
-// Inicializa LocalStorage en la primera visita usando una constante exportada.
-import { INITIAL_STATE } from './storage/seed.js';
-import { initStorage } from './storage/localStorage.js';
-
-initStorage(INITIAL_STATE);
